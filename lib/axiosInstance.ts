348a9d827f1cdab26c6be1d935e401350b54fa99
pxuria <pouriamesram@gmail.com>
2026-07-25 20:45:14 +0330

    fix axios

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const API_DOMAIN = process.env.NEXT_PUBLIC_API_DOMAIN?.includes("/api")
+  ? process.env.NEXT_PUBLIC_API_DOMAIN
+  : process.env.NEXT_PUBLIC_API_DOMAIN + "/api";
+
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_DOMAIN,
+  baseURL: API_DOMAIN,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
